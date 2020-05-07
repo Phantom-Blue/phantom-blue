b3371fb3c395a08eff70610f1a054ed6a1ed6c8f
Angie Spears <angie.spears809@gmail.com>
2020-05-07 16:55:17 -0400

    render map and add pin svg file

--- client/components/mapView/MapView.js
+++ client/components/mapView/MapView.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import ReactMapGl, {
+  Marker,
+  Popup,
+  NavigationControl,
+  FullscreenControl
+} from 'react-map-gl'
+import * as data from '../data/data.json'
+import '../../../secrets'
 
 export const MapView = () => {
-  return <div>HERE GOES THE MAP</div>
+  const [viewport, setViewport] = useState({
+    latitute: 40.7128,
+    longitude: -74.006,
+    width: '100vw',
+    height: '100vh',
+    zoom: 4
+  })
+
+  const [selectedState, setSeletedState] = useState(null)
+
+  return (
+    <div>
+      <ReactMapGl
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        onViewportChange={viewport => {
+          setViewport(viewport)
+        }}
+      />
+    </div>
+  )
 }
 
 export default MapView
