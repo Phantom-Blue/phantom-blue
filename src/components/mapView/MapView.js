9a16d2fc236d9d172c3e859c3cff9be7197887f8
GiselleDZ <giselle@gisellezatonyl.com>
2020-05-08 13:08:16 -0400

    Reset PWA, imported map, working on process.env

---
+++ src/components/mapView/MapView.js
@@ -0,0 +1,65 @@
+import React, {useState, useEffect} from 'react';
+import ReactMapGl, {
+  Marker,
+  // Popup,
+  // NavigationControl,
+  // FullscreenControl
+} from 'react-map-gl';
+import * as data from '../data/data.json';
+
+const markerBtn = {
+  background: 'none',
+  border: 'none',
+};
+
+export const MapView = () => {
+  const [viewport, setViewport] = useState({
+    latitude: 40.7736,
+    longitude: -73.9566,
+    width: '100vw',
+    height: '100vh',
+    zoom: 13,
+  });
+
+  const [selectedState, setSeletedState] = useState(null);
+
+  return (
+    <div>
+      <ReactMapGl
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        onViewportChange={viewport => {
+          setViewport(viewport)
+        }}
+      >
+        {data.newYorkCities.map((borough, idx) => {
+          return (
+            <Marker
+              key={idx}
+              latitude={borough.latitude}
+              longitude={borough.longitude}
+            >
+              <button
+                style={markerBtn}
+                onClick={e => {
+                  e.preventDefault()
+                  setSeletedState(borough)
+                }}
+              >
+                <img
+                  width="50px"
+                  height="50px"
+                  src="/location-pin.png"
+                  alt="city"
+                />
+              </button>
+            </Marker>
+          )
+        })}
+      </ReactMapGl>
+    </div>
+  )
+}
+
+export default MapView
--- src/components/splash.js
+++ src/components/splash.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MapView from './mapView/MapView'
 
 export default () => (
   <section className="hero is-info is-fullheight">
     <div className="hero-body">
       <div className="container">
-        <h1 className="title">
+        <MapView />
+        {/* <h1 className="title">
             Welcome to ReactPWA
-        </h1>
-        <h2 className="subtitle">
+        </h1> */}
+        {/* <h2 className="subtitle">
             An extendable boilerplate built on top of PawJS, for developers
-        </h2>
+        </h2> */}
+        <h1> it works!! </h1>
         <Link to="/home" className="button">Checkout the full demo</Link>
       </div>
     </div>
