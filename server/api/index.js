483e73ba07ebc901873414aed1edd4b6bab91745
Saj King <a.sajking@gmail.com>
2020-05-12 20:08:29 -0400

    Added nearby art functionality to location model, plus added location api route

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,8 @@ router.use('/users', require('./users'))
 
 router.use('/artworks', require('./artworks'))
 
+router.use('/locations', require('./locations'))
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
