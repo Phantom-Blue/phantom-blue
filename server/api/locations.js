5e9a8b73e950bc82bb9405e4be1fe9760bbb11c9
Saj King <a.sajking@gmail.com>
2020-05-13 11:55:45 -0400

    updated seed and radius of nearby art

--- server/api/locations.js
+++ server/api/locations.js
@@ -4,7 +4,7 @@ const {Location} = require('../db/models/Index')
 router.get('/artNearby/:id', async (req, res, next) => {
   try {
     const location = await Location.findByPk(req.params.id)
-    const nearbyArt = await location.getNearbyArt(10000)
+    const nearbyArt = await location.getNearbyArt(1000)
     res.send(nearbyArt)
   } catch (err) {
     next(err)
