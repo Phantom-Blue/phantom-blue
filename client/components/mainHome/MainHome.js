e71dc6d01984cd49048a76a6346d857275741b05
GiselleDZ <giselle@gisellezatonyl.com>
2020-05-14 14:14:16 -0400

    fixed render bug

--- client/components/mainHome/MainHome.js
+++ client/components/mainHome/MainHome.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-return-assign */
 /* eslint-disable no-unused-expressions */
 import React from 'react'
 import {connect} from 'react-redux'
@@ -17,33 +18,32 @@ class MainHome extends React.Component {
     let directionsUrl
     return (
       <div>
-        {this.props.artworks[0]
-          ? this.props.artworks.map(artwork => {
-              ;<div key={artwork.id}>
-                <div>{console.log(artwork.id)}</div>
-                <h1> IT LOADS </h1>
-                <img src={artwork.imageUrl[0]} />
-                <Link to={`/artwork/${this.props.match.params.id}`}>
-                  <button type="button">
-                    <p>{artwork.description}</p>
-                  </button>
-                </Link>
-                {(directionsUrl = generateUrl(artwork.Location.address))}
-                <a
-                  href={directionsUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <h4>TAKE ME THERE</h4>
-                </a>
-              </div>
-            })
-          : ''
-        // <center>
-        //     <h2>L O A D I N G . . .</h2>
-        //     <img src="http://gisellezatonyl.com/images/blobbers-03-newalgos-12-23-13-02-lessframes-600pxw.gif" width="300" />
-        // </center>
-        }
+        {this.props.artworks[0] ? (
+          this.props.artworks.map(artwork => (
+            <div key={artwork.id}>
+              <div>{console.log(artwork.id)}</div>
+              <h1> IT LOADS </h1>
+              <img src={artwork.imageUrl[0]} />
+              <Link to={`/artwork/${this.props.match.params.id}`}>
+                <button type="button">
+                  <p>{artwork.description}</p>
+                </button>
+              </Link>
+              {(directionsUrl = generateUrl(artwork.Location.address))}
+              <a href={directionsUrl} target="_blank" rel="noopener noreferrer">
+                <h4>TAKE ME THERE</h4>
+              </a>
+            </div>
+          ))
+        ) : (
+          <center>
+            <h2>L O A D I N G . . .</h2>
+            <img
+              src="http://gisellezatonyl.com/images/blobbers-03-newalgos-12-23-13-02-lessframes-600pxw.gif"
+              width="300"
+            />
+          </center>
+        )}
       </div>
     )
   }
