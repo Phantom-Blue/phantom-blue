2fc1566aad8d15a6b40270a4af6b2a27f918c485
Saj King <a.sajking@gmail.com>
2020-05-16 15:25:47 -0400

    Cloudinary upload route working

--- server/api/index.js
+++ server/api/index.js
@@ -9,6 +9,8 @@ router.use('/tags', require('./tags'))
 
 router.use('/locations', require('./locations'))
 
+router.use('/images', require('./images'))
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
