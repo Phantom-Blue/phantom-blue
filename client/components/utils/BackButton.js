8a3ff6e54eb03b0268ff39de1ad5eebf9bdd9d20
Angie Spears <angie.spears809@gmail.com>
2020-05-21 12:59:43 -0400

    add back button

--- client/components/utils/BackButton.js
+++ client/components/utils/BackButton.js
@@ -1,14 +1,27 @@
 import React from 'react'
 
+// BUTTON STYLE
+const backButtonStyle = {
+  border: 'none',
+  backgroundColor: 'white',
+  borderRadius: '20px',
+  cursor: 'pointer',
+  color: 'black',
+  fontSize: '20px',
+  marginTop: '20px',
+  textAlign: 'left'
+}
+
 const BackButton = () => {
   return (
     <button
       type="button"
+      style={backButtonStyle}
       onClick={() => {
         history.back()
       }}
     >
-      Back
+      BACK
     </button>
   )
 }
